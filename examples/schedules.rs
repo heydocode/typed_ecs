6ce645ad14fa2de38318bfc456686bdab0715a30
heydocode <heydocode@outlook.fr>
2025-09-06 20:34:12 +0200

    feat(compile-time app): make App easier to preprocess and more ergonomic
    Changes App-centralised approach to App as manager, StaticApp to add plugins without expensive reallocations (as it's a ZST), and RuntimeApp as a runner and allocated ECS.
    
    Closes #3

--- examples/schedules.rs
+++ examples/schedules.rs
@@ -1,6 +1,4 @@
-use std::marker::PhantomData;
-
-use typed_ecs::{app::App, plugin::Plugin, pluginlist::UnallocatedPluginList};
+use typed_ecs::{app::App, plugin::Plugin};
 
 fn main() {
     struct Example(u64);
@@ -13,7 +11,15 @@ fn main() {
             self.0 += 1;
             println!("Counter: {}", self.0);
         }
+        fn update_exit_status(&self, should_exit: &mut typed_ecs::app::ShouldExit) {
+            if self.0 > 5_000_000 {
+                should_exit.request_exit();
+            }
+        }
+        fn on_exit(&mut self) {
+            println!("Graceful shutdown!");
+        }
     }
-    let mut app = App::new(PhantomData::<()>.add_plugin(Example(0)));
-    app.run();
+    
+    App::new().add_plugin::<Example>().build().run();
 }
