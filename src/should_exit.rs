4d0566c05e3ec8706daaaf2a30cb828676cc0757
heydocode <heydocode@outlook.fr>
2026-04-20 20:35:53 +0200

    chore(ShouldExit): Explain its purposes

--- src/should_exit.rs
+++ src/should_exit.rs
@@ -1,3 +1,7 @@
+/// Allows for using an u8 instead of bool,
+/// to not loose 7 bits because of alignment.
+/// Moreover introduces access safety.
+/// E.g. here we can only toggle on, not off.
 pub trait ShouldExit {
     fn request_exit(&mut self);
     fn is_true(&self) -> bool;
